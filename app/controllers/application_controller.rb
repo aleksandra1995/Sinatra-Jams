f128079efdecdf04a43ac06fa45f094b90a446e2
github username <github email address>
2019-07-17 17:31:19 -0400

    finished lab

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,40 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  get "/artists" do
+    @artists = Artist.all
+    erb :index
+  end
+
+  get "/artists/new" do 
+    erb :new
+  end
+
+  post "/artists" do 
+    @artist = Artist.create(params[:artist])
+    redirect to "/artists/#{@artist.id}"
+  end
+
+  get "/artists/:id" do 
+    @artist = Artist.find(params[:id])
+    erb :show
+  end
+
+  get "/artists/:id/edit" do 
+    @artist = Artist.find(params[:id])
+    erb :edit
+  end
+
+  put "/artists/:id" do 
+    @artist = Artist.find(params[:id])
+    @artist.update(params[:artist])
+    redirect to "/artists/#{@artist.id}"
+  end
+
+  delete "/artists/:id" do 
+    @artist = Artist.find(params[:id])
+    @artist.destroy
+    redirect to "/artists"
+  end
+
 end
